bcf16eb279e111ef964b2b00a8553e8abb3c7d92
DaphneLai <Daphnelai.q@gmail.com>
2025-02-03 14:19:19 -0500

    hai added everything

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Hero from './components/Hero.vue';
+import HelloWorld from './components/Hero.vue'
+import ProjectCard from './components/Project-card.vue';
+import TheWelcome from './components/Project-card.vue'
+</script>
+
+
+<template>
+  <body>
+    <Hero />
+    
+  </body>
+
+</template>
+
+<style scoped>
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex; 
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="greetings">
+    <h3>Hi, I'm Daphne Lai</h3>
+    <h1>
+      I’m a designer who crafts gratifying experiences with storytelling and creative problem solving.
+    </h1>
+    <button type="button" class="button-txt" >❁ Let’s connect</button>
+
+  </div>
+  <div class="centre logo">
+    <img alt="3D me" src="https://s3.us-east-2.amazonaws.com/daphnelai.github.io/3D-me.svg"/>
+  </div>
+</template>
+
+<style scoped>
+.button-txt {
+  color: #5F5A5A;
+  font-family: "Instrument Sans";
+  font-weight: 100;
+  font-size: large;
+  padding: 0.8rem 1.5rem;
+  border-color: #CBC6C6;
+  border-radius: 80rem;
+
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+.centre {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
